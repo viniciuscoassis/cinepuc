567f2a1801e9839bfd2a1bd8b08024665eea7a80
vinicius <vinicius.coassis@gmail.com>
2023-06-23 14:44:02 -0300

    feat: verificação se cadeira está vazia antes de adicionar

--- backend/src/main.c
+++ backend/src/main.c
@@ -55,7 +55,6 @@ int main() {
                     adicionarAssento(listaAssentos, idSessao, idCadeira);
                     printf("Assento adicionado com sucesso!\n");
                 }
-
                 exibirListaAssentos(listaAssentos);
                 break;
             case 2:
